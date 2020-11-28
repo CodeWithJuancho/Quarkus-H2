964f54e57f472c5e060c00eabbfe9e59269e20ac
Juan Sánchez <juan.sanchez@bitingbit.com>
2020-11-28 21:26:16 +0000

    H2 mockup project finished

---
+++ backend/src/main/java/org/acme/Test.java
@@ -0,0 +1,20 @@
+package org.acme;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Test {
+
+    @Id
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+CREATE TABLE User
+(
+    Id        int,
+    FirstName varchar(255),
+    LastName  varchar(255)
+);
+
+
+INSERT INTO User (Id, FirstName, LastName)
+VALUES (1, 'Foo', 'Boo');
